5ad7dd8226197dcd1ca8c307f3f5e2ce7cc92756
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2025-03-07 17:21:04 +0530

    practice

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
-output "public_ip" {
-    value = aws_instance.db.public_ip[0]
-}
+# output "public_ip" {
+#     value = aws_instance.db.public_ip
+# }
 
-output "private_ip" {
-    value = aws_instance.db.private_ip[0]
+# output "private_ip" {
+#     value = aws_instance.db.private_ip
+# }
+
+output "aws_info" {
+  value = aws_instance.db
 }
