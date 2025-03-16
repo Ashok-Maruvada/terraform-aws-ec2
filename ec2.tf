831ac9d820c3b320b43da6ae4b28c57cd562f9e8
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2025-03-16 09:41:43 +0530

    practice

--- ec2.tf
+++ ec2.tf
@@ -3,4 +3,7 @@ resource "aws_instance" "db" {
     ami = var.ami_id
     vpc_security_group_ids = var.security_group_id
     instance_type = each.value
+    tags = {
+        name = each.key
+    }
 }
